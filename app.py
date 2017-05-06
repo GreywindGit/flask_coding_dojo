2ef9482e62233cd5277b6921d033e97a24e1c116
GreywindGit <GreywindGit@users.noreply.github.com>
2017-05-06 12:58:37 +0200

    Apply counter in all server functions

--- app.py
+++ app.py
@@ -2,39 +2,45 @@ from flask import Flask, render_template, redirect
 import helper
 app = Flask(__name__)
 
+counter = helper.get_stats('request_counts.txt')
+
 
 @app.route('/')
 def index():
-    return "Hello :)"
+    return render_template('index.html')
 
 
 @app.route('/request-counter', methods=['GET'])
 def get_counter():
+    global counter
     counter['GET'] += 1
     return redirect('/')
 
 
 @app.route('/request-counter', methods=['POST'])
 def post_counter():
+    global counter
     counter['POST'] += 1
     return redirect('/')
 
 
 @app.route('/request-counter', methods=['PUT'])
 def put_counter():
+    global counter
     counter['PUT'] += 1
     return redirect('/')
 
 
 @app.route('/request-counter', methods=['DELETE'])
 def del_counter():
+    global counter
     counter['DELETE'] += 1
     return redirect('/')
 
 
 @app.route('/statistics')
 def statistics():
-    stats = helper.get_stats('request_counts.txt')
+    stats = counter
     return render_template('statistics.html', stats=stats)
 
 if __name__ == "__main__":
