5ea8f9a13be735b174b0f76cd96aabfd3fc47206
GreywindGit <GreywindGit@users.noreply.github.com>
2017-05-06 12:26:04 +0200

    Add server functions

--- app.py
+++ app.py
@@ -6,5 +6,34 @@ app = Flask(__name__)
 def index():
     return "Hello :)"
 
+
+@app.route('/request-counter', methods=['GET'])
+def get_counter():
+    counter['GET'] += 1
+    return redirect('/')
+
+
+@app.route('/request-counter', methods=['POST'])
+def post_counter():
+    counter['POST'] += 1
+    return redirect('/')
+
+
+@app.route('/request-counter', methods=['PUT'])
+def put_counter():
+    counter['PUT'] += 1
+    return redirect('/')
+
+
+@app.route('/request-counter', methods=['DELETE'])
+def del_counter():
+    counter['DELETE'] += 1
+    return redirect('/')
+
+
+@app.route('/statistics')
+def statistics():
+    return render_template('statistics.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
